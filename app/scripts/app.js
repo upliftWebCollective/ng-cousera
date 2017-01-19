d13abdbc062bfac846ec6f61b2442ff533b3d372
Rick G <lacunainc04@gmail.com>
2017-01-18 22:40:31 -0800

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,10 @@
+(function(){
+  angular.module('courseraApp', [])
+    .controller('introCtlr', function($scope){
+      $scope.title = "Ng-Coursera";
+      $scope.desc = "Repository for Single Page Web Applications with AngularJS course."
+  });
+
+
+
+}) ();
