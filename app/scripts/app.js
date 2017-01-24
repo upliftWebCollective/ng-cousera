f422308d48bc6c77d998afeb02ff557323ccb86b
Rick G <lacunainc04@gmail.com>
2017-01-23 23:34:50 -0800

    Initialize Module 2 solution page with ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,22 @@
 (function(){
-  angular.module('courseraApp', [])
+  angular.module('courseraApp', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'main.html'
+        })
+        .state('module2-solution', {
+          url: '/module2-solution',
+          templateUrl: 'solutions/m2-sol.html'
+        })
+
+    $urlRouterProvider.otherwise('/')
+  }])
     .controller('introCtlr', function($scope){
       $scope.title = "Ng-Coursera";
       $scope.desc = "Repository for Single Page Web Applications with AngularJS course."
-  });
-
+  })
 
 
 }) ();
