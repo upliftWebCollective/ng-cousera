2a609207c25305dab3bebe40899a8ed3d9ef1598
Rick G <lacunainc04@gmail.com>
2017-02-05 21:29:55 -0800

    Complete Module 3 solution

--- app/solutions/module3/module.js
+++ app/solutions/module3/module.js
@@ -3,21 +3,31 @@ angular.module('NarrowItDown', []);
 
 angular.module('NarrowItDown')
   .controller('Module3', Module3Parent)
-  .controller('NarrowItDownController', NarrowItDownController)
-  .directive('foundItems', resultDirective)
+
+    .controller('NarrowItDownController', NarrowItDownController)
+    .directive('narrowDown', NarrowItDownDirective)
+
+    .directive('foundItems', resultDirective)
+    .controller('foundItemsController', foundItemsController)
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService){
     var vm = this;
     vm.found = '';
-
+    vm.removeItem = function(index){
+      vm.found.splice(index, 1)
+    }
     vm.displayMatched = function(userInput){
-      //Add this function to the search button with ng-click
-      console.log('userInput: ', userInput);
       vm.found = MenuSearchService.getMatchedMenuItems(userInput);
       return vm.found;
-      // console.log('vm.found'. vm.found);
     }
+  }
+  function NarrowItDownDirective() {
+    return {
+      restrict: 'AE',
+      templateUrl: 'solutions/module3/views/narrowDown.html',
+      controller: 'NarrowItDownController as narrow',
+    };
   }
 
   Module3Parent.$inject = ['MenuSearchService']
@@ -25,14 +35,19 @@ angular.module('NarrowItDown')
     var vm = this;
     vm.title = "Module 3 Solution";
   }
+  function foundItemsController(){
+    var dvm = this;
+  }
   function resultDirective() {
     return {
       restrict: 'AE',
       scope: {
-        itemLoaded: '<'
+        itemLoaded: '<',
+        onRemove: '&'
       },
       templateUrl: 'solutions/module3/views/found.html',
-      controller: 'NarrowItDownController as found'
+      controller: 'foundItemsController as result',
+      bindToController: true,
     };
   }
 
